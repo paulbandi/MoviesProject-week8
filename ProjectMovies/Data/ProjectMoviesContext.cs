158d05513fc7b1393bbdc8f12a89c770bdf926e6
paul.bandi <paul.bandi@emanuel.ro>
2021-06-10 01:01:23 +0300

    Adding files

---
+++ ProjectMovies/Data/ProjectMoviesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectMovies.Models;
+
+namespace ProjectMovies.Data
+{
+    public class ProjectMoviesContext : DbContext
+    {
+        public ProjectMoviesContext (DbContextOptions<ProjectMoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjectMovies.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ ProjectMovies/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectMovies.Models
+{
+    public class Movie
+    {
+        public Guid Id {get; set;}
+
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+
+    }
+}
